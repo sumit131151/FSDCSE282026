8cb7f2991c9e1d65b07ec6f09cbba43ba33edb09
sumit131151 <er.sumit1311@gmail.com>
2026-07-28 13:29:21 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// function sum(a, b) {
+//     return a + b;
+// }
+// sum(5, 10);
+// console.log(sum(5, 10));
+// // square root of sum
+// console.log(Math.sqrt(sum(5, 10)));
+
+let a = 34;
+if (a > 20){
+let a = 45;
+    console.log("a inside a="+a);
+}
+console.log("a outside a="+a);
+
